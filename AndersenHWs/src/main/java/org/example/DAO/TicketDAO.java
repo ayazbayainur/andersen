c68ea9d46c3717ee32119c4df6d539a586d1a187
Ainur <ayazbay.ainur@mail.ru>
2024-10-17 21:29:10 +0500

    changes after CR

--- AndersenHWs/src/main/java/org/example/DAO/TicketDAO.java
+++ AndersenHWs/src/main/java/org/example/DAO/TicketDAO.java
@@ -1,8 +1,11 @@
 package org.example.DAO;
 
+import org.example.Properties;
 import org.example.Ticket;
 import org.example.TicketType;
+import org.example.User;
 
+import java.sql.*;
 import java.time.LocalDate;
 
 public class TicketDAO {
@@ -21,5 +24,41 @@ public class TicketDAO {
 
     public String updateTicketType(String id, TicketType ticketType){
         return "UPDATE ticket SET ticket_type = '" + ticketType + "' WHERE id = '" + id + "';";
+    }
+
+    public static void main(String[] args){
+        Properties properties = new Properties();
+        TicketDAO ticketDAO = new TicketDAO();
+
+        try(
+                Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_ticket_service_db",
+                        properties.username, properties.password);
+                Statement stmt = conn.createStatement();
+        ){
+            Ticket ticket = new Ticket("5", "4", TicketType.WEEK, LocalDate.parse("2024-10-01"));
+            stmt.executeUpdate(ticketDAO.saveTicket(ticket));
+            ResultSet rs2 = stmt.executeQuery(ticketDAO.findTicketById("2"));
+            while(rs2.next()) {
+                System.out.print("id: "+rs2.getString("id")+", ");
+                System.out.print("user id: "+rs2.getString("user_id")+", ");
+                System.out.print("ticket type: "+rs2.getString("ticket_type")+", ");
+                System.out.print("creation date: "+rs2.getString("creation_date"));
+                System.out.println();
+            }
+            String userId = "3";
+            ResultSet rs3 = stmt.executeQuery(ticketDAO.findTicketByUserId(userId));
+            System.out.println("Tickets with user id = " + userId);
+            while(rs3.next()) {
+                System.out.print("id: "+rs3.getString("id")+", ");
+                System.out.print("user id: "+rs3.getString("user_id")+", ");
+                System.out.print("ticket type: "+rs3.getString("ticket_type")+", ");
+                System.out.print("creation date: "+rs3.getString("creation_date"));
+                System.out.println();
+            }
+            stmt.executeUpdate(ticketDAO.updateTicketType("1", TicketType.DAY));
+        } catch (SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+
     }
 }
--- AndersenHWs/src/main/java/org/example/DAO/UserDAO.java
+++ AndersenHWs/src/main/java/org/example/DAO/UserDAO.java
@@ -1,7 +1,11 @@
 package org.example.DAO;
 
+import org.example.Properties;
+import org.example.Ticket;
+import org.example.TicketType;
 import org.example.User;
 
+import java.sql.*;
 import java.time.LocalDate;
 
 public class UserDAO {
@@ -17,6 +21,35 @@ public class UserDAO {
     // added "ON DELETE CASCADE" when creating table so that the user's tickets are also deleted
     public String deleteById(String id){
         return "DELETE FROM user WHERE id = '" + id + "';";
+    }
+
+    public static void main(String[] args){
+        Properties properties = new Properties();
+        UserDAO userDAO = new UserDAO();
+        try(
+                Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_ticket_service_db",
+                        properties.username, properties.password);
+                Statement stmt = conn.createStatement();
+        ){
+            User user = new User("6", "aida6", LocalDate.parse("2020-01-01"), User.Role.ADMIN, "752672565", "aida6@mail.com");
+            stmt.executeUpdate(userDAO.saveUser(user));
+            String userId = "3";
+            ResultSet rs1 = stmt.executeQuery(userDAO.findUserById(userId));
+            System.out.println("Users with id = " + userId);
+            while(rs1.next()) {
+                System.out.print("id: "+rs1.getString("id")+", ");
+                System.out.print("name: "+rs1.getString("name")+", ");
+                System.out.print("creation date: "+rs1.getString("creation_date") + ", ");
+                System.out.print("role: "+rs1.getString("role")+", ");
+                System.out.print("phone: "+rs1.getString("phone")+", ");
+                System.out.print("email: "+rs1.getString("email"));
+                System.out.println();
+            }
+            stmt.executeUpdate(userDAO.deleteById("2"));
+        } catch (SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+
     }
 
 }
